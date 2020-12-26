42fcb20bd74c4712e24ce671ac7eb45f9004a084
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-26 14:40:21 -0800

    Some archives 08
    Some archives 08

---
+++ _src/CODES1/VersionControlDemo/app/src/main/java/com/example/robpercival/versioncontroldemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.robpercival.versioncontroldemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String user = "Rob";
+
+        // Facebook is great!
+
+        // Actually, it's not so good
+
+        // I'm done!!
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
