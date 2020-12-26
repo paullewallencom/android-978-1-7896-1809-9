e1080746c09ff7a66ccbcc0a936009a554af21b2
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-26 14:18:45 -0800

    Some archives 03
    Some archives 03

---
+++ _src/CODES1/02.07/ClickDemo/app/src/main/java/com/robpercival/clickdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.robpercival.clickdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+
+        Log.i("Info", "The button was clicked!");
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
